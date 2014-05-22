cf91a016a501c931f07fd6842152547538505cff
LittleWolf30510 <littlewolf30510@gmail.com>
2014-05-22 10:07:34 -0700

    Initial push of Rankup to git

---
+++ SQRankup/src/com/regalphoenixmc/SQRankup/Database.java
@@ -0,0 +1,286 @@
+package com.regalphoenixmc.SQRankup;
+
+import java.sql.*;
+
+public class Database {
+	public String player;
+	public String server;
+	public String world;
+	public int x, y, z;
+	public static String driver = "com.mysql.jdbc.Driver";
+	public static String hostname = "localhost";
+	public static String port = "3306";
+	public static String db_name = "minecraft";
+	public static String username = "minecraft";
+	public static String password = "R3b!rth!ng";
+	public static Connection cntx = null;
+	public static String dsn = ("jdbc:mysql://" + hostname + ":" + port + "/" + db_name);
+
+	public static void setUp() {
+		String Database_table = "CREATE TABLE IF NOT EXISTS RANKUP (" 
+				+ "`name` VARCHAR(32) NOT NULL," 
+				+ "`kills` int(16) DEFAULT 0," 
+				+ "`lastkill` VARCHAR(32) DEFAULT NULL,"
+				+ "`lastkilltime` bigint(18) DEFAULT 0," 
+				+ "PRIMARY KEY (`name`)" 
+				+ ")";
+		getContext();
+		try {
+			Driver driver = (Driver) Class.forName(Database.driver).newInstance();
+			DriverManager.registerDriver(driver);
+		} catch (Exception e) {
+			System.out.println("[SQDatabases] Driver error: " + e);
+		}
+		Statement s = null;
+		
+		try {
+			s = cntx.createStatement();
+			s.executeUpdate(Database_table);
+			System.out.println("[SQDatabase] Table check/creation sucessful");
+		} catch (SQLException ee) {
+			System.out.println("[SQDatabase] Table Creation Error");
+		} finally {
+			close(s);
+		}
+
+	}
+
+	// return 0 if they haven't killed anyone
+	public static int getKills(String player) {
+		System.out.print("[Rankup] Getting Kills"); // TODO: Debugging ish
+		if(!getContext()) System.out.println("something is wrong!");
+		Statement s = null;
+		try {
+			s = cntx.createStatement();
+			ResultSet rs = s.executeQuery("SELECT kills FROM RANKUP WHERE name = " + "\'" + player + "\'");
+
+			while (rs.next()) {
+				int kills = rs.getInt("kills");
+				return kills;
+			}
+			s.close();
+		} catch (SQLException e) {
+			System.out.print("[SQDatabase] SQL Error" + e.getMessage());
+		} catch (Exception e) {
+			System.out.print("[SQDatabase] SQL Error (Unknown)");
+			e.printStackTrace();
+		} finally {
+			close(s);
+		}
+		return 0;
+	}
+
+	public static void setNewKills(String player, int kills) {
+
+		if(!getContext()) System.out.println("something is wrong!");
+		PreparedStatement s = null;
+			try {
+				s = cntx.prepareStatement("INSERT INTO RANKUP (name, kills, lastkill, lastkilltime) values (?,?,?,?)");
+				s.setString(1, player);
+				s.setInt(2, kills);
+				s.setString(3, "placeholder");
+				s.setDouble(4, 0);
+				s.execute();
+				s.close();
+			} catch (SQLException e) {
+				System.out.print("[SQDatabase] SQL Error" + e.getMessage());
+			} catch (Exception e) {
+				System.out.print("[SQDatabase] SQL Error (Unknown)");
+				e.printStackTrace();
+			} finally {
+				close(s);
+			}
+			
+	}
+
+	public static void setKills(String player, int kills) {
+
+		if(!getContext()) System.out.println("something is wrong!");
+		PreparedStatement s = null;
+		try {
+			s = cntx.prepareStatement("UPDATE RANKUP SET kills = ? WHERE NAME = ?");
+			s.setInt(1, kills);
+			s.setString(2, player);
+			s.execute();
+			s.close();
+		} catch (SQLException e) {
+			System.out.print("[SQDatabase] SQL Error" + e.getMessage());
+		} catch (Exception e) {
+			System.out.print("[SQDatabase] SQL Error (Unknown)");
+			e.printStackTrace();
+		} finally {
+			close(s);
+		}
+	}
+
+	// return null if they haven't killed anyone
+
+	public static String getLastKill(String player) {
+		System.out.print("[Rankup] Getting Last Kill"); // TODO: Debugging ish
+		if(!getContext()) System.out.println("something is wrong!");
+		Statement s = null;
+			try {
+				s = cntx.createStatement();
+				ResultSet rs = s.executeQuery("SELECT lastkill FROM RANKUP WHERE name = " + "\'" + player + "\'");
+
+				while (rs.next()) {
+					String kill = rs.getString("lastkill");
+					return kill;
+				}
+				s.close();
+			} catch (SQLException e) {
+				System.out.print("[SQDatabase] SQL Error" + e.getMessage());
+			} catch (Exception e) {
+				System.out.print("[SQDatabase] SQL Error (Unknown)");
+				e.printStackTrace();
+			} finally {
+				close(s);
+			}
+		return null;
+	}
+
+	public static void setLastKill(String player, String killed) {
+		if(!getContext()) System.out.println("something is wrong!");
+		PreparedStatement s = null;
+		try {
+			s = cntx.prepareStatement("UPDATE RANKUP SET lastkill = ? WHERE NAME = ?");
+			s.setString(1, killed);
+			s.setString(2, player);
+			s.execute();
+			s.close();
+		} catch (SQLException e) {
+			System.out.print("[SQDatabase] SQL Error" + e.getMessage());
+		} catch (Exception e) {
+			System.out.print("[SQDatabase] SQL Error (Unknown)");
+			e.printStackTrace();
+		} finally {
+			close(s);
+		}
+	}
+
+	// return 0 if they haven't killed anyone
+	public static double getLastKillTime(String player) {
+		System.out.print("[Rankup] Getting Last Kill Time"); // TODO: Debugging
+																// ish
+		if(!getContext()) System.out.println("something is wrong!");
+		Statement s = null;
+		try {
+			s = cntx.createStatement();
+			ResultSet rs = s.executeQuery("SELECT lastkilltime FROM RANKUP WHERE name = " + "\'" + player + "\'");
+
+			while (rs.next()) {
+				double kills = rs.getDouble("lastkilltime");
+				return kills;
+			}
+			s.close();
+		} catch (SQLException e) {
+			System.out.print("[SQDatabase] SQL Error" + e.getMessage());
+		} catch (Exception e) {
+			System.out.print("[SQDatabase] SQL Error (Unknown)");
+			e.printStackTrace();
+		} finally {
+			close(s);
+		}
+		return 0;
+	}
+
+	public static void setLastKillTime(String player, double time) {
+		if(!getContext()) System.out.println("something is wrong!");
+		PreparedStatement s = null;
+		try {
+			s = cntx.prepareStatement("UPDATE RANKUP SET lastkilltime = ? WHERE NAME = ?");
+			s.setDouble(1, time);
+			s.setString(2, player);
+			s.execute();
+			s.close();
+		} catch (SQLException e) {
+			System.out.print("[SQDatabase] SQL Error" + e.getMessage());
+		} catch (Exception e) {
+			System.out.print("[SQDatabase] SQL Error (Unknown)");
+			e.printStackTrace();
+		} finally {
+			close(s);
+		}
+	}
+	
+	//checks if they are in the DB
+	public static boolean hasKey(String player) {
+		System.out.print("[Rankup] Getting Last Kill Time"); // TODO: Debugging
+																// ish
+		if(!getContext()) System.out.println("something is wrong!");
+		Statement s = null;
+		try {
+			s = cntx.createStatement();
+			ResultSet rs = s.executeQuery("SELECT * FROM RANKUP WHERE name = " + "\'" + player + "\'");
+
+			if(rs.next()) {
+				s.close();
+				return true;
+			}
+			s.close();
+		} catch (SQLException e) {
+			System.out.print("[SQDatabase] SQL Error" + e.getMessage());
+		} catch (Exception e) {
+			System.out.print("[SQDatabase] SQL Error (Unknown)");
+			e.printStackTrace();
+		} finally {
+			close(s);
+		}
+		return false;
+	}
+
+	// don't bother with these, just helpers for me
+	public static void incrementKills(String player) {
+		setKills(player, getKills(player) + 1);
+	}
+
+	public static void setLastKillTimeToCurrent(String player) {
+		setLastKillTime(player, System.currentTimeMillis());
+	}
+
+	// Connection Setter Method I need
+	public static boolean getContext() {
+
+		try {
+			if (cntx == null || cntx.isClosed() || !cntx.isValid(1)) {
+				if (cntx != null && !cntx.isClosed()) {
+					try {
+						cntx.close();
+					} catch (SQLException e) {
+						System.out.print("Exception caught");
+					}
+					cntx = null;
+				}
+				if ((Database.username.equalsIgnoreCase("")) && (Database.password.equalsIgnoreCase(""))) {
+					cntx = DriverManager.getConnection(Database.dsn);
+				} else
+					cntx = DriverManager.getConnection(Database.dsn, Database.username, Database.password);
+
+				if (cntx == null || cntx.isClosed())
+					return false;
+			}
+
+			return true;
+		} catch (SQLException e) {
+			System.out.print("Error could not Connect to db " + Database.dsn + ": " + e.getMessage());
+		}
+		return false;
+	}
+	
+	  private static void close(Statement s)
+	  {
+	    if (s == null) {
+	      return;
+	    }
+	    try
+	    {
+	      s.close();
+	      cntx.close();
+	    }
+	    catch (Exception e)
+	    {
+	      e.printStackTrace();
+	    }
+	  }
+
+}
---
+++ SQRankup/src/com/regalphoenixmc/SQRankup/SQRankup.java
@@ -0,0 +1,276 @@
+
+package com.regalphoenixmc.SQRankup;
+
+import java.util.Arrays;
+import java.util.List;
+
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.permission.Permission;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import ru.tehkode.permissions.PermissionGroup;
+import ru.tehkode.permissions.PermissionManager;
+import ru.tehkode.permissions.PermissionUser;
+import ru.tehkode.permissions.bukkit.PermissionsEx;
+
+public class SQRankup extends JavaPlugin implements Listener {
+
+	public static Permission permission = null;
+	public static Economy economy = null;
+	public static int index = 0;
+	PermissionManager pex;
+	public static List<PermissionGroup> pexGroups;
+
+	public void onEnable() {
+
+		saveDefaultConfig();
+		getServer().getPluginManager().registerEvents(this, this);
+		setupEconomy();
+		Database.setUp();
+		pex = PermissionsEx.getPermissionManager();
+		pexGroups = Arrays.asList(pex.getGroups());
+	}
+
+	// command
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+		if (cmd.getName().equalsIgnoreCase("rankup") && sender instanceof Player) {
+			Player p = (Player) sender;
+			p.sendMessage(ChatColor.GOLD + "Getting rankup data for you! Please wait.");
+			String rank = getRank(p.getName());
+			String nextRank = getNextRank(rank);
+			PermissionUser user = pex.getUser(p);
+
+			if (rank.equalsIgnoreCase("SETTLER")) {
+				if (args.length == 0) {
+					sender.sendMessage(ChatColor.RED + " You need to choose a rank path! do /rankup Pirate or /rankup Colonist");
+					return true;
+				} else if (args[0].equalsIgnoreCase("Colonist")) {
+					nextRank = "COLONIST";
+				} else if (args[0].equalsIgnoreCase("Pirate")) {
+					nextRank = "PIRATE";
+					// TODO: townKick(p);
+				} else {
+					sender.sendMessage("You must choose either pirate or colonist. Invalid argument.");
+					return true;
+				}
+			}
+
+			if (nextRank.equalsIgnoreCase("SETTLER")) {
+				sender.sendMessage(ChatColor.RED + "To rank up to settler, you must apply to the server on our minecraft forums thread");
+				sender.sendMessage(ChatColor.GOLD + "http://tinyurl.com/starquestapps");
+				return true;
+			}
+			int moneyRequirement = getMonetaryCost(nextRank);
+			int killsRequirement = getKillRequirement(nextRank);
+
+			int killsFound = Database.getKills(sender.getName());
+			double moneyFound = economy.getBalance(sender.getName());
+
+			if (killsFound >= killsRequirement && moneyFound >= moneyRequirement) {
+				getServer().broadcastMessage(ChatColor.RED + sender.getName() + " has ranked up to " + nextRank.toString().toLowerCase() + "!");
+				user.addGroup(nextRank);
+				user.removeGroup(rank);
+				economy.withdrawPlayer(sender.getName(), moneyRequirement);
+				Database.setKills(sender.getName(), killsFound - killsRequirement);
+			} else {
+				sender.sendMessage("Current money: " + moneyFound + " Required Money: " + moneyRequirement + " Current Kills: " + killsFound
+						+ " Required Kills: " + killsRequirement);
+			}
+			return true;
+		}
+		if (cmd.getName().equalsIgnoreCase("addapp") && sender.hasPermission("SQRankup.addApplication")) {
+			String rank = getRank(args[0]);
+			String nextRank = getNextRank(rank);
+			PermissionUser user = pex.getUser(getServer().getPlayer(args[1]));
+			if (args.length >= 1) {
+				if (getServer().getOfflinePlayer(args[0]) != null) {
+					getServer().broadcastMessage(ChatColor.RED + args[0] + " has ranked up to settler!");
+					getServer().broadcastMessage(
+							ChatColor.RED + "Are you a " + ChatColor.GREEN + "Refugee" + ChatColor.RED + "? Rank up to " + ChatColor.DARK_GREEN + "Settler"
+									+ ChatColor.RED + " like " + args[0] + " did!");
+					getServer().broadcastMessage(
+							ChatColor.RED + "Visit " + ChatColor.BLUE + "http://tinyurl.com/starquestapps" + ChatColor.RED + " to apply for Settler rank!");
+
+					user.addGroup(nextRank);
+					user.removeGroup(rank);
+					if (args.length >= 2 && getServer().getOfflinePlayer(args[1]) != null) {
+						economy.depositPlayer(args[1], 10000);
+						getServer().broadcastMessage(
+								ChatColor.GOLD + args[1] + ChatColor.RED + " brought " + args[0] + " to the server and earned 10000 for doing so!");
+					}
+					return true;
+				}
+				sender.sendMessage("This player has never played before. Did you get their name right?");
+				return false;
+			}
+			sender.sendMessage("Needs an argument.");
+			return false;
+		}
+		return false;
+
+	}
+
+	// listener for kills
+	@EventHandler
+	public void onPlayerKill(PlayerDeathEvent event) {
+
+		Player killer = event.getEntity().getKiller();
+		if (killer instanceof Player) {
+			String name = killer.getName();
+			String lastKill = Database.getLastKill(name);
+			if (lastKill != null && lastKill.equals(event.getEntity().getName())) {
+				if (System.currentTimeMillis() - Database.getLastKillTime(name) < 180000) {
+					killer.sendMessage("You've already killed this guy in the last half hour. Lay off, dude. This kill wasn't counted.");
+					return;
+				}
+			}
+			if (!Database.hasKey(name)) {
+				Database.setNewKills(name, 0);
+			}
+			Database.incrementKills(name);
+			Database.setLastKill(name, event.getEntity().getName());
+			Database.setLastKillTimeToCurrent(name);
+			killer.sendMessage(ChatColor.RED + "This kill was counted in the ranking system.");
+			return;
+
+		}
+
+	}
+
+	// method to get the next rank on the rank structure
+	public String getNextRank(String rank) {
+
+		switch (rank) {
+			case "REFUGEE":
+				return "SETTLER";
+			case "SETTLER":
+				return "AMBIG";
+			case "PIRATE":
+				return "CORSAIR";
+			case "CORSAIR":
+				return "BUCCANEER";
+			case "BUCCANEER":
+				return "WARLORD";
+			case "COLONIST":
+				return "CITIZEN";
+			case "CITIZEN":
+				return "AFFLUENT";
+			case "AFFLUENT":
+				return "TYCOON";
+			case "WARLORD":
+			case "TYCOON":
+				return null;
+
+			default:
+				return null;
+		}
+	}
+
+	// method for getting monetary cost of rankup
+	public int getMonetaryCost(String rank) {
+
+		switch (rank) {
+			case "REFUGEE":
+				return getConfig().getInt("refugee.cost");
+			case "SETTLER":
+				return getConfig().getInt("settler.cost");
+			case "PIRATE":
+				return getConfig().getInt("pirate.cost");
+			case "CORSAIR":
+				return getConfig().getInt("corsair.cost");
+			case "BUCCANEER":
+				return getConfig().getInt("buccaneer.cost");
+			case "COLONIST":
+				return getConfig().getInt("colonist.cost");
+			case "CITIZEN":
+				return getConfig().getInt("citizen.cost");
+			case "AFFLUENT":
+				return getConfig().getInt("affluent.cost");
+			case "WARLORD":
+				return getConfig().getInt("warlord.cost");
+			case "TYCOON":
+				return getConfig().getInt("tycoon.cost");
+			default:
+				return 0;
+		}
+	}
+
+	public int getKillRequirement(String rank) {
+
+		switch (rank) {
+			case "REFUGEE":
+				return getConfig().getInt("refugee.kills");
+			case "SETTLER":
+				return getConfig().getInt("settler.kills");
+			case "PIRATE":
+				return getConfig().getInt("pirate.kills");
+			case "CORSAIR":
+				return getConfig().getInt("corsair.kills");
+			case "BUCCANEER":
+				return getConfig().getInt("buccaneer.kills");
+			case "COLONIST":
+				return getConfig().getInt("colonist.kills");
+			case "CITIZEN":
+				return getConfig().getInt("citizen.kills");
+			case "AFFLUENT":
+				return getConfig().getInt("affluent.kills");
+			case "WARLORD":
+				return getConfig().getInt("warlord.kills");
+			case "TYCOON":
+				return getConfig().getInt("tycoon.kills");
+			default:
+				return 0;
+		}
+	}
+
+	public String getRank(String player) {
+
+		for (PermissionGroup group : pexGroups) {
+			for (PermissionUser user : group.getUsers()) {
+				if (user.getName().equalsIgnoreCase(player)) {
+					if (getNextRank(group.getName().toUpperCase()) != null) {
+						return group.getName().toUpperCase();
+					}
+				}
+			}
+		}
+
+		return null;
+	}
+
+	public PermissionGroup getGroup(String player) {
+
+		for (PermissionGroup group : pexGroups) {
+			for (PermissionUser user : group.getUsers()) {
+				if (user.getName().equalsIgnoreCase(player)) {
+					if (getNextRank(group.getName().toUpperCase()) != null) {
+						return group;
+					}
+				}
+			}
+		}
+
+		return null;
+	}
+
+	// Handling economy provider onEnable (Vault)
+	private boolean setupEconomy() {
+
+		RegisteredServiceProvider<Economy> economyProvider = getServer().getServicesManager().getRegistration(Economy.class);
+		if (economyProvider != null) {
+			economy = (Economy) economyProvider.getProvider();
+		}
+		return economy != null;
+	}
+
+}
