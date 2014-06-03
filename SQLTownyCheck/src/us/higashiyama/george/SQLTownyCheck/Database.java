829db022d9e1dbb8fa402b5a41dd692944e33333
LittleWolf30510 <littlewolf30510@gmail.com>
2014-06-03 12:56:11 -0700

    Commit of adding multi-town support.

--- SQLTownyCheck/src/us/higashiyama/george/SQLTownyCheck/Database.java
+++ SQLTownyCheck/src/us/higashiyama/george/SQLTownyCheck/Database.java
@@ -1,3 +1,4 @@
+
 package us.higashiyama.george.SQLTownyCheck;
 
 import java.sql.Connection;
@@ -9,109 +10,117 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Database {
-		public String player;
-		public String server;
-		public String world;
-		public int x, y, z;
-		public static String driverString = "com.mysql.jdbc.Driver";
-		public static String hostname = "192.99.20.8";
-		public static String port = "3306";
-		public static String db_name = "towny";
-		public static String username = "minecraft";
-		public static String password = "R3b!rth!ng";
-		public static Connection cntx = null;
-		public static String dsn = ("jdbc:mysql://" + hostname + ":" + port + "/" + db_name);
 
-		public static void setUp() {
-			try {
-				Driver driver = (Driver) Class.forName(driverString).newInstance();
-				System.out.println(driver);
-				DriverManager.registerDriver(driver);
-			} catch (Exception e) {
-				System.out.println("[CCDBs] Driver error: " + e);
-			}
+	public String player;
+	public String server;
+	public String world;
+	public int x, y, z;
+	public static String driverString = "com.mysql.jdbc.Driver";
+	public static String hostname = "192.99.20.8";
+	public static String port = "3306";
+	public static String db_name = "towny";
+	public static String username = "minecraft";
+	public static String password = "R3b!rth!ng";
+	public static Connection cntx = null;
+	public static String dsn = ("jdbc:mysql://" + hostname + ":" + port + "/" + db_name);
+
+	public static void setUp() {
+
+		try {
+			Driver driver = (Driver) Class.forName(driverString).newInstance();
+			System.out.println(driver);
+			DriverManager.registerDriver(driver);
+		} catch (Exception e) {
+			System.out.println("[CCDBs] Driver error: " + e);
 		}
-		
+	}
 
+	public static int towns(String name) {
 
-		
-		public static boolean hasKey(String name) {
-			if(!getContext()) System.out.println("Context didn't work sucessfully");
-			PreparedStatement s = null;
-			try {
-				String query = "SELECT `name` FROM acualis_residents WHERE `town` != & UNION SELECT `name` FROM asteroidbelt_residents WHERE `town` != & UNION SELECT `name` FROM boletarian_residents WHERE `town` != & UNION SELECT `name` FROM boskevine_residents WHERE `town` != & UNION SELECT `name` FROM ceharram_residents WHERE `town` != & UNION SELECT `name` FROM defalos_residents WHERE `town` != & UNION SELECT `name` FROM digitalia_residents WHERE `town` != & UNION SELECT `name` FROM drakos_residents WHERE `town` != & UNION SELECT `name` FROM emera_residents WHERE `town` != & UNION SELECT `name` FROM iffrizar_residents WHERE `town` != & UNION SELECT `name` FROM inaris_residents WHERE `town` != & UNION SELECT `name` FROM kelakaria_residents WHERE `town` != & UNION SELECT `name` FROM krystallos_residents WHERE `town` != & UNION SELECT `name` FROM quavara_residents WHERE `town` != & UNION SELECT `name` FROM regalis_residents WHERE `town` != & UNION SELECT `name` FROM valadro_residents WHERE `town` != &";
-				query = query.replace("&", "\"\"");
-				System.out.println(query);
-				s = cntx.prepareStatement(query);
-				ResultSet rs = s.executeQuery();
-				while(rs.next()) {
-					if(rs.getString("name").equalsIgnoreCase(name)) {
-						return true;
-					}
-					
-				} 
-				s.close();
-				return false;
+		if (!getContext())
+			System.out.println("Context didn't work sucessfully");
+		PreparedStatement s = null;
+		int towns = 0;
+		try {
+			String query = "SELECT `name`AS player, COUNT(*) as totalTowns FROM( " + "SELECT`name`, `town`  FROM acualis_residents WHERE `town` != & UNION "
+					+ "SELECT`name`, `town`  FROM asteroidbelt_residents WHERE `town` != & UNION "
+					+ "SELECT`name`, `town`  FROM boletarian_residents WHERE `town` != & UNION "
+					+ "SELECT`name`, `town`  FROM boskevine_residents WHERE `town` != & UNION "
+					+ "SELECT`name`, `town`  FROM ceharram_residents WHERE `town` != & UNION "
+					+ "SELECT`name`, `town`  FROM defalos_residents WHERE `town` != & UNION "
+					+ "SELECT`name`, `town`  FROM digitalia_residents WHERE `town` != & UNION "
+					+ "SELECT`name`, `town`  FROM drakos_residents WHERE `town` != & UNION "
+					+ "SELECT`name`, `town`  FROM emera_residents WHERE `town` != & UNION "
+					+ "SELECT`name`, `town`  FROM iffrizar_residents WHERE `town` != & UNION "
+					+ "SELECT`name`, `town`  FROM inaris_residents WHERE `town` != & UNION "
+					+ "SELECT`name`, `town`  FROM kelakaria_residents WHERE `town` != & UNION "
+					+ "SELECT`name`, `town`  FROM krystallos_residents WHERE `town` != & UNION "
+					+ "SELECT`name`, `town`  FROM quavara_residents WHERE `town` != & UNION "
+					+ "SELECT`name`, `town`  FROM regalis_residents WHERE `town` != & UNION "
+					+ "SELECT`name`, `town`  FROM valadro_residents WHERE `town` != &" + ") AS query WHERE `name` = ?";
+			query = query.replace("&", "\"\"");
+			System.out.println(query);
+			s = cntx.prepareStatement(query);
+			s.setString(1, name);
+			ResultSet rs = s.executeQuery();
+			while (rs.next()) {
+				towns = rs.getInt("totalTowns");
 
-					
-
-				
-			} catch (SQLException e) {
-				System.out.print("[CCDB] SQL Error" + e.getMessage());
-			} catch (Exception e) {
-				System.out.print("[CCDB] SQL Error (Unknown)");
-				e.printStackTrace();
-			} finally {
-				close(s);
 			}
-		
-			return false;
-	
-			
+			s.close();
+			return towns;
+
+		} catch (SQLException e) {
+			System.out.print("[CCDB] SQL Error" + e.getMessage());
+		} catch (Exception e) {
+			System.out.print("[CCDB] SQL Error (Unknown)");
+			e.printStackTrace();
+		} finally {
+			close(s);
 		}
 
-		public static boolean getContext() {
-			
-			try {
-				if (cntx == null || cntx.isClosed() || !cntx.isValid(1)) {
-					if (cntx != null && !cntx.isClosed()) {
-						try {
-							cntx.close();
-						} catch (SQLException e) {
-							System.out.print("Exception caught");
-						}
-						cntx = null;
-					}
-					if ((username.equalsIgnoreCase("")) && (password.equalsIgnoreCase(""))) {
-						cntx = DriverManager.getConnection(dsn);
-					} else
-						cntx = DriverManager.getConnection(dsn, username, password);
+		return towns;
 
-					if (cntx == null || cntx.isClosed())
-						return false;
+	}
+
+	public static boolean getContext() {
+
+		try {
+			if (cntx == null || cntx.isClosed() || !cntx.isValid(1)) {
+				if (cntx != null && !cntx.isClosed()) {
+					try {
+						cntx.close();
+					} catch (SQLException e) {
+						System.out.print("Exception caught");
+					}
+					cntx = null;
 				}
+				if ((username.equalsIgnoreCase("")) && (password.equalsIgnoreCase(""))) {
+					cntx = DriverManager.getConnection(dsn);
+				} else
+					cntx = DriverManager.getConnection(dsn, username, password);
 
-				return true;
-			} catch (SQLException e) {
-				System.out.print("Error could not Connect to db " + dsn + ": " + e.getMessage());
+				if (cntx == null || cntx.isClosed())
+					return false;
 			}
-			return false;
+
+			return true;
+		} catch (SQLException e) {
+			System.out.print("Error could not Connect to db " + dsn + ": " + e.getMessage());
 		}
-		
-		
-		  private static void close(Statement s)
-		  {
-		    if (s == null) {
-		      return;
-		    }
-		    try
-		    {
-		      s.close();
-		    }
-		    catch (Exception e)
-		    {
-		      e.printStackTrace();
-		    }
-		  }
+		return false;
+	}
+
+	private static void close(Statement s) {
+
+		if (s == null) {
+			return;
+		}
+		try {
+			s.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
--- SQLTownyCheck/src/us/higashiyama/george/SQLTownyCheck/SQLTownyCheck.java
+++ SQLTownyCheck/src/us/higashiyama/george/SQLTownyCheck/SQLTownyCheck.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
@@ -22,7 +23,7 @@ public class SQLTownyCheck extends JavaPlugin implements Listener {
 	@EventHandler
 	public void onPlayerCommand(PlayerCommandPreprocessEvent e) {
 
-		String p = e.getPlayer().getName();
+		Player p = e.getPlayer();
 		String command = e.getMessage().replace("/", "");
 		ArrayList<String> args = new ArrayList<String>(Arrays.asList(command.split(" ")));
 
@@ -36,27 +37,55 @@ public class SQLTownyCheck extends JavaPlugin implements Listener {
 				e.setCancelled(true);
 				return;
 			}
-			if (Database.hasKey(args.get(2)) && Bukkit.getPlayer(args.get(2)).hasPermission("town.join")) {
+
+			if (!canJoinTown(getServer().getPlayer(args.get(2)))) {
 				Bukkit.getPlayer(args.get(2)).sendMessage(
 						ChatColor.AQUA + "You cannot be invited to a town. You are already in one or do not have permissions.");
 				e.getPlayer().sendMessage(ChatColor.AQUA + "That user is already in a town. If this is a mistake please contact a dev");
 				e.setCancelled(true);
+				return;
 			}
 		}
 
 		if ((args.get(1).equalsIgnoreCase("join") && (args.get(0).equalsIgnoreCase("town") || args.get(0).equalsIgnoreCase("t")))) {
-			if (Database.hasKey(p) && Bukkit.getPlayer(p).hasPermission("town.join")) {
+			if (!canJoinTown(p)) {
 				e.getPlayer().sendMessage(ChatColor.AQUA + "You are already in a town or cannot join one.");
 				e.setCancelled(true);
+				return;
 			}
 		}
 
 		if ((args.get(1).equalsIgnoreCase("new") && (args.get(0).equalsIgnoreCase("town") || args.get(0).equalsIgnoreCase("t")))) {
-			if (Database.hasKey(p)) {
+			if (!canStartTown(p)) {
 				e.getPlayer().sendMessage(ChatColor.AQUA + "You cannot start a town. You are already in one.");
 				e.setCancelled(true);
+				return;
 			}
 		}
 
+	}
+
+	public static boolean canJoinTown(Player p) {
+
+		int currentTowns = Database.towns(p.getName());
+		if ((currentTowns == 0) && p.hasPermission("town.join")) {
+			return true;
+		} else if ((currentTowns > 0 && p.hasPermission("town.multiple." + (currentTowns + 1)))) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static boolean canStartTown(Player p) {
+
+		int currentTowns = Database.towns(p.getName());
+		if ((currentTowns == 0) && p.hasPermission("town.join")) {
+			return true;
+		} else if ((currentTowns > 0 && p.hasPermission("town.startmultiple." + (currentTowns + 1)))) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
