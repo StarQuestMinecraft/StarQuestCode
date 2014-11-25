db0eda6eaeda310c9daba89a27fa72d50e396c43
dibujaron <ddp@integrity.com>
2014-11-24 22:35:56 -0500

    removed comments

--- SQConnectionPool/src/com/starquestminecraft/sqcp/Pool.java
+++ SQConnectionPool/src/com/starquestminecraft/sqcp/Pool.java
@@ -35,23 +35,6 @@ public class Pool extends JavaPlugin {
 		}
 	}
 
-	/*
-	 * Plugins that should use this API
-	 * 
-	 * - SQShops - SQRankup - SQDuties - SQBungeeMovecraft - Redeem
-	 * 
-	 * Pre-pool stats: 12 connections * (12 planets + 3 space + 1 limbo + 1
-	 * belt) = 204 cntx 12 connections * (10 planets + 3 space + 1 limbo) = 168
-	 * cntx
-	 * 
-	 * Post-pool stats (assuming max pool size of 3): 9 connections * (12
-	 * planets + 3 space + 1 limbo + 1 belt) = 153 cntx 9 connections * (10
-	 * planets + 3 space + 1 limbo) = 126 cntx
-	 * 
-	 * Maximum servers with this pool: 22 1 limbo + 1 belt + 4 space + 16
-	 * planets
-	 */
-
 	public static SharedConnection checkOutConnection() {
 		totalChecks++;
 		for (SharedConnection s : connections) {
