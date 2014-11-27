8914a0d2dcd1745ab57763abdefc668b7a6d0f32
LittleWolf30510 <LittleWolf30510@gmail.com>
2014-11-27 08:24:16 -0800

    Added trading node class to prepare for location based trading.

--- SQRankup/src/com/regalphoenixmc/SQRankup/Currency.java
+++ SQRankup/src/com/regalphoenixmc/SQRankup/Currency.java
@@ -5,20 +5,29 @@ import org.bukkit.entity.Player;
 
 public abstract class Currency {
 
-	// override
+	private TradingNode node = null;
+
 	public String getAlias() {
 
 		return "";
 	}
 
-	// Also needs override
 	public RankupPlayer canPurchase(Player player, double money, int kills, Perk perk) {
 
 		return null;
 	}
 
-	// make sure to override for extending classes
 	public void purchase(Player player, RankupPlayer rp, double money, int kills) {
 
+	}
+
+	public TradingNode getNode() {
+
+		return node;
+	}
+
+	public void setNode(TradingNode node) {
+
+		this.node = node;
 	}
 }
