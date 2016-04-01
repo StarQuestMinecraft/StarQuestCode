b401f0061cfaf3423c15431f0ec16b76b5efa72f
martinjonsson01 <martinjonsson01@gmail.com>
2016-04-02 00:20:59 +0200

    SQSmoothCraft - Added missile fireball
    - Ammo now required to fire missile from missile launcher
    - Missile is a fireball atm, might be changed later

--- SQSmoothCraft/src/com/martinjonsson01/sqsmoothcraft/missile/MissileDetection.java
+++ SQSmoothCraft/src/com/martinjonsson01/sqsmoothcraft/missile/MissileDetection.java
@@ -65,4 +65,30 @@ public class MissileDetection {
 		
 	}
 	
+	public static Block inFrontOfDispenser(Block sign){
+		BlockFace direction = null;
+		BlockFace launcherDirection = null;
+		
+		if (sign.getRelative(BlockFace.NORTH).getType() == Material.SPONGE)
+			direction = BlockFace.NORTH;
+		if (sign.getRelative(BlockFace.EAST).getType() == Material.SPONGE)
+			direction = BlockFace.EAST;
+		if (sign.getRelative(BlockFace.WEST).getType() == Material.SPONGE)
+			direction = BlockFace.WEST;
+		if (sign.getRelative(BlockFace.SOUTH).getType() == Material.SPONGE)
+			direction = BlockFace.SOUTH;
+			
+		if (sign.getRelative(direction).getRelative(BlockFace.NORTH).getType() == Material.DISPENSER)
+			launcherDirection = BlockFace.NORTH;
+		if (sign.getRelative(direction).getRelative(BlockFace.EAST).getType() == Material.DISPENSER)
+			launcherDirection = BlockFace.EAST;
+		if (sign.getRelative(direction).getRelative(BlockFace.WEST).getType() == Material.DISPENSER)
+			launcherDirection = BlockFace.WEST;
+		if (sign.getRelative(direction).getRelative(BlockFace.SOUTH).getType() == Material.DISPENSER)
+			launcherDirection = BlockFace.SOUTH;
+		
+		return sign.getRelative(direction).getRelative(launcherDirection).getRelative(launcherDirection).getRelative(launcherDirection);
+		
+	}
+	
 }
