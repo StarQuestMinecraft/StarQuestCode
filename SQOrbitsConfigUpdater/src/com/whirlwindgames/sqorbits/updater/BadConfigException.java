46e0253f60216a8ec449d0a108e878f316de7a0e
lawrencethefrog <lawrencethefrog@users.noreply.github.com>
2016-06-22 13:50:45 +0100

    Initial commit of SQOrbitsConfigUpdater

---
+++ SQOrbitsConfigUpdater/src/com/whirlwindgames/sqorbits/updater/BadConfigException.java
@@ -0,0 +1,8 @@
+package com.whirlwindgames.sqorbits.updater;
+
+public class BadConfigException extends Exception {
+	/**
+	 * A custom Exception thrown when an error occurs loading data from the config.
+	 */
+	private static final long serialVersionUID = 1L;
+}
